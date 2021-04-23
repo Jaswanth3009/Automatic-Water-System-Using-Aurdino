a6b2982cd16bd987ceadeac7e4b3dcb5e99183a0
Jaswanth3009 <79711020+Jaswanth3009@users.noreply.github.com>
2021-04-23 13:31:13 +0530

    Update arduino-plant-watering.ino

--- arduino-plant-watering.ino
+++ arduino-plant-watering.ino
@@ -1,192 +1,112 @@
-#include <avr/wdt.h>
-#include <LiquidCrystal.h>
-#include <DS3232RTC.h>    
-#include <Time.h>         
-#include <Wire.h>         
-#include "config.h"
-#include "ds3232.h"
-#include "buttons.h"
-#include "lcd.h"
-#include "frames.h"
-#include "busy_wait.h"
+#include<LiquidCrystal.h>
 
-pump_t pump = {
-  .state = PUMP_OFF,
-  .state_start = 0,
-  .runtime = WATER_RUNTIME,
-  .count = 0
-};
 
-void setup() {
-  uint8_t arrow[8] = {
-    0b00000,
-    0b00100,
-    0b00010,
-    0b11111,
-    0b00010,
-    0b00100,
-    0b00000,
-    0b00000
-  };
-  lcd.createChar(0, arrow);
-  lcd.begin(COLS, ROWS);
-
-  lcd.print("WATER PUMPER 1.0");
-  uint16_t _delay = 1000;
-  uint16_t _char_delay = _delay / COLS;
-
-  uint8_t i, j = 0;
-  for (j = 0; j < COLS; ++j) {
-    if (j > 0) {
-      busyWait(_char_delay);
-    }
-
-    lcd.setCursor(0, 1);
-    for (i = 0; i < j; ++i) {
-      lcd.print('-');
-    }
-    lcd.write(byte(0));
-   }
-
-  setSyncProvider(RTC.get);
-  if (timeStatus() != timeSet) {
-     lcd.print("Error: Invalid time status");
-  }
-
-  delay(1000);
-
-  button_init(1000, 0);
-  button_init(1000, 270);
-  button_init(1000, 270 + 390);
-
-  readAlarm(ALARM_2, &alarm2_def);
-
-  // clear alarm flags
-  RTC.alarm(ALARM_1);
-  RTC.alarm(ALARM_2);
-
-  RTC.squareWave(SQWAVE_NONE);
-
-  current_time = now();
-
-  RTC.setAlarm(ALM1_EVERY_SECOND, 0, 0, 0, 0); 
+ #define moisture_sensorPin A0
+ #define float_switchPin A1
+ #define motorPin 4
+ #define soil_statusPin 2
+ #define tank_statusPin 3
  
-  RTC.alarmInterrupt(ALARM_1, true);
+ LiquidCrystal lcd(13,12,11,10,9,8);
+ 
+ const int avg_moisture = 800; 
 
-  pinMode(RTC_INT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(RTC_INT_PIN), alarmChange, FALLING);
 
-  pinMode(FLOAT_SWITCH_INT_PIN, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(FLOAT_SWITCH_INT_PIN), floatSwitchInterrupt, CHANGE);
 
-  pinMode(PUMP_PIN, OUTPUT);
-  digitalWrite(PUMP_PIN, LOW);
+void setup() 
+{
+ Serial.begin(9600);
+ lcd.begin(16,2);
+ lcd.clear();
+ lcd.setCursor(0,0);
+ lcd.print(" AUTOMATIC ");
+ lcd.setCursor(0,1);
+ lcd.print(" IRRIGATION S/M ");
+ delay(2000);
+ 
+ 
+ pinMode(moisture_sensorPin,INPUT);
+ pinMode(float_switchPin,INPUT);
+ pinMode(motorPin,OUTPUT);
+ pinMode(soil_statusPin,OUTPUT);
+ pinMode(tank_statusPin,OUTPUT);
+ 
+ digitalWrite(motorPin,LOW);
+ digitalWrite(soil_statusPin,LOW);
+ digitalWrite(tank_statusPin,LOW);
+
+
 }
 
-void pump_on() {
-  if (digitalRead(FLOAT_SWITCH_INT_PIN) == FLOAT_SWITCH_OFF) {
-    return;
-  }
+void loop() 
+{
 
-  pump.state = PUMP_ON;
-  pump.state_start = millis();
-  pump.count++;
-  digitalWrite(PUMP_PIN, HIGH);
+lcd.begin(16,2);
+ 
+ 
+ 
+ lcd.setCursor(0,0);
+ lcd.print(" MOISTURE - ");
+ 
+ if(analogRead(moisture_sensorPin) > avg_moisture){
+ lcd.print("HIGH");
+ digitalWrite(soil_statusPin,HIGH);}
+ 
+ if(analogRead(moisture_sensorPin) < avg_moisture){
+ lcd.print(" LOW");
+ digitalWrite(soil_statusPin,LOW);}
 
-  /* configure watchdog to 1 second */
-  watchdog_init(WDTO_1S);
+
+
+ 
+ lcd.setCursor(0,1);
+ lcd.print("TANK LEVEL- ");
+ 
+ if( digitalRead(float_switchPin) == HIGH){
+ lcd.print("HIGH");
+ digitalWrite(tank_statusPin,LOW);}
+ 
+ if( digitalRead(float_switchPin) == LOW){
+ lcd.print(" LOW");
+ digitalWrite(tank_statusPin,HIGH);}
+
+
+ 
+ digitalWrite(motorPin,LOW);
+ 
+ 
+ if(analogRead(moisture_sensorPin) < avg_moisture && digitalRead(float_switchPin) == HIGH)
+ { 
+ 
+ while(analogRead(moisture_sensorPin) < avg_moisture && digitalRead(float_switchPin) == HIGH)
+ { 
+ lcd.setCursor(0,0);
+ lcd.print(" MOISTURE - LOW");
+ lcd.setCursor(0,1);
+ lcd.print(" MOTOR IS ON "); 
+ digitalWrite(soil_statusPin,LOW);
+ digitalWrite(tank_statusPin,LOW); 
+ digitalWrite(motorPin,HIGH); 
+ }
+ 
+ if(analogRead(moisture_sensorPin) > avg_moisture){
+ lcd.setCursor(0,0);
+ lcd.print(" MOISTURE - HIGH");
+ lcd.setCursor(0,1);
+ lcd.print(" MOTOR - OFF "); 
+ digitalWrite(soil_statusPin,HIGH);
+ digitalWrite(motorPin,LOW);
+ delay(3000);}
+ 
+ if(digitalRead(float_switchPin) == LOW){
+ lcd.setCursor(0,0);
+ lcd.print(" TANK LEVEL- LOW");
+ lcd.setCursor(0,1);
+ lcd.print(" MOTOR - OFF "); 
+ digitalWrite(tank_statusPin,HIGH);
+ digitalWrite(motorPin,LOW);
+ delay(3000);}
+
 }
-
-void pump_off() {
-  wdt_disable();
-
-  digitalWrite(PUMP_PIN, LOW);
-  pump.state = PUMP_OFF;
-  pump.state_start = millis();
-}
-
-uint16_t get_pump_count() {
-  return pump.count;
-}
-
-uint16_t get_pump_runtime() {
-  return pump.runtime;
-}
-
-void set_pump_runtime(uint16_t runtime) {
-  pump.runtime = runtime;
-}
-
-
-void alarmChange() {
-  alarmCalled = 1;
-}
-
-void floatSwitchInterrupt() {
-  if (digitalRead(FLOAT_SWITCH_INT_PIN) == FLOAT_SWITCH_OFF && pump.state == PUMP_ON) {
-    pump_off();
-  }
-}
-
-void watchdog_init(uint8_t mask) {
-  /* Clear the reset flag. */
-  MCUSR &= ~(1 << WDRF);
-
-  
-  WDTCSR |= (1<<WDCE) | (1<<WDE);
-
-  /* set new watchdog timeout prescaler value */
-  WDTCSR = mask;
-
-  /* Enable the WD interrupt (note no reset). */
-  WDTCSR |= _BV(WDIE);
-}
-
-
-ISR(WDT_vect) {
-  if ((millis() - pump.state_start) >= pump.runtime) {
-    pump_off();
-  }
-}
-
-void loop() {
-  if (alarmCalled) {
-    alarmCalled = 0;
-    if (RTC.alarm(ALARM_1)) {
-      handle_alarm1();
-    }
-
-    if (RTC.alarm(ALARM_2) && is_alarm_active(ALARM_2)) {
-      handle_alarm2();
-    }
-  }
-
-  switch (get_active_frame()) {
-    case FRAME_0:
-      /* root frame */
-      frame0();
-    break;
-
-    case FRAME_1:
-      /* menu frame */
-      frame1();
-    break;
-
-    case FRAME_2:
-      /* menu option frame */
-      frame2();
-      break;
-
-    case FRAME_3:
-      frame3();
-      break;
-  }
-
-  set_prev_frame(get_active_frame());
-
-  if (get_next_frame() != -1) {
-    set_active_frame(get_next_frame());
-    set_next_frame(-1);
-    set_last_draw();
-  }
+ delay(500);
 }
